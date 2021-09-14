91d8b809563fb3362cb3e25c80bb2e297d84a0bc
Hanna Jantunen <hannajant@gmail.com>
2021-09-14 19:38:43 +0300

    osa2.12

---
+++ osa2/maiden-tiedot/src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Filter = ({ filter, handleChange }) => {
+  return (
+    <div>
+      find countries <input value={filter} onChange={handleChange}/>
+    </div>
+  )
+}
+
+const Country = ({ country }) => {
+  console.log(country)
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <div>
+        capital {country.capital}
+        <br></br>
+        population {country.population}
+        <h2>languages</h2>
+        <ul>
+          {country.languages.map((language) => <li key={language.name}>{language.name}</li>)}
+        </ul>
+        <img src={country.flag} alt="flag of country" width="150" />
+      </div>
+    </div>
+  )
+}
+
+const Countries = ({ filter, countries}) => {
+  const toShow = countries.filter((country) => country.name.toLowerCase().includes(filter.toLowerCase()))
+
+  if (toShow.length == 1){
+    return (
+      <Country country={toShow[0]} />
+      )
+  } else if(toShow.length < 11) {
+    return (
+      <div>
+        {toShow.map((country) => <p key={country.alpha3Code}>{country.name}</p>)}
+      </div>
+    )
+  } else {
+    return <p>Too many matches, specify another filter</p>
+  }
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios.get('https://restcountries.eu/rest/v2/all')
+    .then(response => {
+      setCountries(response.data)
+    })
+  }, [])
+
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  return (
+    <div>
+      <Filter filter={filter} handleChange={handleFilterChange} />
+      {countries.length ? 
+        <Countries filter={filter} countries={countries} />
+        : <p>not found</p>
+      }
+    </div>
+  )
+}
+
+export default App
