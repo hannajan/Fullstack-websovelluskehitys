8d12d5b0e1dd770a4086500ce8590266a3883aff
Hanna Jantunen <hannajant@gmail.com>
2021-09-21 19:11:07 +0300

    blogs controller

--- blogilista/app.js
+++ blogilista/app.js
@@ -2,59 +2,22 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  
-  blogSchema.set('toJSON', {
-      transform: (document, returnedObject) => {
-          returnedObject.id = returnedObject._id.toString()
-          delete returnedObject._id
-          delete returnedObject.__v
-      }
-  })
-  
-  const Blog = mongoose.model('Blog', blogSchema)
- 
-  mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-      logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-      logger.error('error connection to MongoDB:', error.message)
-    })
-  
-  
-  app.use(cors())
-  app.use(express.json())
-  
-  
-  app.get('/', (request, response) => {
-      response.send('<h1>Bloglist app</h1>')
-  })
-  
-  app.get('/api/blogs', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-  app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
+mongoose.connect(config.MONGODB_URI)
+.then(() => {
+    logger.info('connected to MongoDB')
+})
+.catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+})
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
