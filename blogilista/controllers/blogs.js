6184b06eae08ae0579c980ba4ac0d761b7ba39bf
Hanna Jantunen <hannajant@gmail.com>
2021-09-21 19:16:04 +0300

    model module

--- blogilista/controllers/blogs.js
+++ blogilista/controllers/blogs.js
@@ -1,22 +1,5 @@
 const blogsRouter = require('express').Router()
-const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  
-  blogSchema.set('toJSON', {
-      transform: (document, returnedObject) => {
-          returnedObject.id = returnedObject._id.toString()
-          delete returnedObject._id
-          delete returnedObject.__v
-      }
-  })
-  
-  const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('../models/blog')
 
 blogsRouter.get('/', (request, response) => {
   Blog
