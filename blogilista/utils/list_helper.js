7028d0ae1661b6fe8c7483bf784053414535d492
Hanna Jantunen <hannajant@gmail.com>
2021-09-23 11:58:49 +0300

    osa4.4

--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const totalLikes = blogs.reduce((sum, blog) => {
+        return blog.likes + sum
+    }, 0)
+    return totalLikes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
