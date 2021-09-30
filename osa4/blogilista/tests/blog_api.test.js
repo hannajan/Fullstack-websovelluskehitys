646562e38370c1e16b2886de8a5d311bdd82c8d6
Hanna Jantunen <hannajant@gmail.com>
2021-09-30 12:41:56 +0300

    osa4.10

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -27,6 +27,27 @@ test('blog has variable named id', async () => {
     expect(response.body[0].id).toBeDefined()
 })
 
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: "Test Blog",
+        author: "Mikki Hiirulainen",
+        url: "https://reactpatterns.com/",
+        likes: 6
+    }
+    
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).toContain('Test Blog')
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
