b7af51896b7ac2d6aaeef77f67be4815d7b20396
Hanna Jantunen <hannajant@gmail.com>
2021-10-01 21:46:32 +0300

    osa4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,9 +5,11 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
+const { response } = require('express')
 
 mongoose.connect(config.MONGODB_URI)
 .then(() => {
@@ -21,6 +23,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
