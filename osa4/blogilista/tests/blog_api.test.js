b47b25a419872d21652ffaed5ab4c112b926ae1e
Hanna Jantunen <hannajant@gmail.com>
2022-03-08 13:56:11 +0200

    osa4.23

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -7,11 +7,32 @@ const api = supertest(app)
 
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const blog = require('../models/blog')
 
 describe('when there are initially some blogs in db', () => {
     beforeEach(async () => {
         await Blog.deleteMany({})
-        await Blog.insertMany(helper.initialBlogs)
+        await User.deleteMany({})
+
+        const testUsers = await helper.generateTwoUsers()
+    
+        let user1  = await testUsers[0].save()
+        let user2 = await testUsers[1].save()
+        
+        let blogsForUser1 = []
+        await helper.user1Blogs.forEach(blog => {
+            const newBlog = {...blog, user: user1}
+            blogsForUser1 = blogsForUser1.concat(newBlog)
+        })
+        let blogsForUser2 = []
+        await helper.user2Blogs.forEach(blog => {
+            const newBlog = {...blog, user: user1}
+            blogsForUser2 = blogsForUser2.concat(newBlog)
+        })
+
+        await Blog.insertMany(blogsForUser1)
+        await Blog.insertMany(blogsForUser2)
+
     })
     
     test('blogs are returned as json', async () => {
@@ -22,7 +43,7 @@ describe('when there are initially some blogs in db', () => {
     
     test('correct number of blogs is returned', async () => {
         const response = await api.get('/api/blogs')
-        expect(response.body).toHaveLength(helper.initialBlogs.length)
+        expect(response.body).toHaveLength(helper.initialLength)
     })
     
     test('blog has variable named id', async () => {
@@ -30,27 +51,44 @@ describe('when there are initially some blogs in db', () => {
         expect(response.body[0].id).toBeDefined()
     })
     
-    describe('adding a new blog', () => {
-      test('a valid blog can be added', async () => {
-        const newBlog = {
+    describe('when logging in is required', () => {
+        let tokenBearer = ''
+        beforeEach(async () => {
+
+            const credentials = {
+                username: 'testuser1',
+                password: 'secret'
+            }
+
+            const result = await api
+              .post('/api/login')
+              .send(credentials)
+
+            tokenBearer = `Bearer ${result.body.token}`
+        })
+
+      describe('adding a new blog', () => {
+        test('a valid blog can be added', async () => {
+          const newBlog = {
             title: "Test Blog",
             author: "Mikki Hiirulainen",
             url: "https://reactpatterns.com/",
             likes: 6
-        }
+          }
     
-        await api
+          await api
             .post('/api/blogs')
             .send(newBlog)
+            .set('Authorization', tokenBearer)
             .expect(201)
             .expect('Content-Type', /application\/json/)
     
-        const blogsAtEnd = await helper.blogsInDb()
-        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+          const blogsAtEnd = await helper.blogsInDb()
+          expect(blogsAtEnd).toHaveLength(helper.initialLength + 1)
     
-        const titles = blogsAtEnd.map(blog => blog.title)
-        expect(titles).toContain('Test Blog')
-      })  
+          const titles = blogsAtEnd.map(blog => blog.title)
+          expect(titles).toContain('Test Blog')
+        })  
 
         test('if likes are not defined they are set to 0', async () => {
             const newBlog = {
@@ -62,6 +100,7 @@ describe('when there are initially some blogs in db', () => {
             const response = await api
                 .post('/api/blogs')
                 .send(newBlog)
+                .set('Authorization', tokenBearer)
                 .expect(201)
                 .expect('Content-Type', /application\/json/)
         
@@ -80,6 +119,7 @@ describe('when there are initially some blogs in db', () => {
             await api
             .post('/api/blogs')
             .send(newBlog)
+            .set('Authorization', tokenBearer)
             .expect(400)
         })
         
@@ -93,39 +133,55 @@ describe('when there are initially some blogs in db', () => {
             await api
             .post('/api/blogs')
             .send(newBlog)
+            .set('Authorization', tokenBearer)
             .expect(400)
         })
-    })
 
-    describe('deletion of a blog', () => {
+        test('return status code 401 if token is missing', async () => {
+            newBlog = {
+                title: 'Token missing',
+                author: 'Mikki Hiirulainen',
+                url: 'https://reactpattens.com/',
+                likes: 0
+            }
+
+            await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(401)
+        })
+      })
+
+      describe('deletion of a blog', () => {
         test('a blog can be deleted with valid id', async () => {
             const blogsAtStart =  await helper.blogsInDb()
             const blogToDelete = blogsAtStart[0]
 
             await api
                 .delete(`/api/blogs/${blogToDelete.id}`)
+                .set('Authorization', tokenBearer)
                 .expect(204)
 
             const blogsAtEnd = await helper.blogsInDb()
-            expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+            expect(blogsAtEnd).toHaveLength(helper.initialLength - 1)
 
             const titles = blogsAtEnd.map(blog => blog.title)
             expect(titles).not.toContain(blogToDelete.title)
         })
 
-        test('returns status code 400 if id invalid', async () => {
+        test('returns status code 401 if id invalid', async () => {
             const invalidId = 'foo'
 
             await api
                 .delete(`/api/blogs/${invalidId}`)
-                .expect(400)
+                .expect(401)
 
             const blogsAtEnd = await helper.blogsInDb()
-            expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+            expect(blogsAtEnd).toHaveLength(helper.initialLength)
         })
-    })
+      })
 
-    describe('updating blog', () => {
+      describe('updating blog', () => {
         test('likes can be updated', async () => {
             const blogsAtStart = await helper.blogsInDb()
             const blogToUpdate = blogsAtStart[0]
@@ -139,6 +195,7 @@ describe('when there are initially some blogs in db', () => {
 
             expect(response.body.likes).toBe(100)
         })
+      })
     })
 })
 
--- osa4/blogilista/tests/test_helper.js
+++ osa4/blogilista/tests/test_helper.js
@@ -1,5 +1,54 @@
+const bcrypt = require('bcrypt')
+const { config } = require('dotenv')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const user1 = {
+  username: 'testuser1',
+  name: 'Test User',
+  password: 'secret',
+}
+
+const user2 = {
+  username: 'testuser2',
+  nama: 'Test User',
+  password: 'salainen',
+}
+
+const user1Blogs = [
+  {
+    title: 'Blog 1',
+    author: 'Marshmallow',
+    url: 'www.url.url',
+    likes: 9,
+  },
+  {
+    title: 'Blog 2',
+    author: 'Lilypad',
+    url: 'www.url.url',
+    likes: 2,
+  }
+]
+
+const user2Blogs = [{
+  title: 'Blog 3',
+  author: 'Marshmallow',
+  url: 'www.url.url',
+  likes: 0
+}]
+
+const initialLength = user1Blogs.length + user2Blogs.length
+
+const generateTwoUsers = async () => {
+  let userArray = []
+  const passwordHash1 = await bcrypt.hash(user1.password, 10)
+  const result1 = await new User({ username: user1.username, name: user1.name, passwordHash: passwordHash1, blogs: []})
+  userArray = userArray.concat(result1)
+  const passwordHash2 = await bcrypt.hash(user2.password, 10)
+  const result2 = await new User({ username: user2.username, name: user2.name, passwordHash: passwordHash2, blogs: []})
+  userArray = userArray.concat(result2)
+  return userArray
+}
 
 const initialBlogs = [
     {
@@ -52,6 +101,10 @@ const usersInDb = async () => {
 
 module.exports = {
     initialBlogs,
+    user1Blogs,
+    user2Blogs,
+    initialLength,
+    generateTwoUsers,
     blogsInDb,
     usersInDb
 }
