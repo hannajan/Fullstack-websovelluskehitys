25584e2c3a6fad9a2809a8fbd97f9866ecd7e257
Hanna Jantunen <hannajant@gmail.com>
2022-03-09 16:28:55 +0200

    osa5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+
+
+  const [showFullView, setShowFullView] = useState(false)
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 2,
+      borderRadius: 5,
+      marginBottom: 5
+    }
+
+    const hideFullView = { display: showFullView ? 'none' : ''}
+    const showFull = { display: showFullView ? '' : 'none'}
+
+  const toggleView = () => {
+    setShowFullView(!showFullView)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideFullView}>
+        {blog.title} {blog.author}
+        <button onClick={toggleView}>view</button>
+      </div>
+      <div style={showFull}>
+        {blog.title} {blog.author} <></>
+        <button onClick={toggleView}>hide</button> <br/>
+        {blog.url} <br/>
+        likes <></> {blog.likes} <button>like</button> <br/>
+        {blog.user.name}
+      </div>
+    </div>
+  ) 
+}
 
 export default Blog
