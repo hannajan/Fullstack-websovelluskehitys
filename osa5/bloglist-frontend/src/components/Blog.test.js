92eb11339b00209843f94584043d95892b0c8fb1
Hanna Jantunen <hannajant@gmail.com>
2022-03-10 14:29:41 +0200

    osa5.15

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -55,6 +55,18 @@ describe('<Blog />', () => {
     expect(div).toHaveTextContent('www.react.fi')
     expect(div).toHaveTextContent('likes 3')
   })
+
+  test('when like button is pressed twice handler is called twice', () => {
+    render(<Blog blog={blog} user={user} handleLike={mockLikeHandler} handleRemove={mockRemoveHandler} />)
+
+    const viewButton = screen.getByText('view')
+    userEvent.click(viewButton)
+    const likeButton = screen.getByText('like')
+    userEvent.click(likeButton)
+    userEvent.click(likeButton)
+
+    expect(mockLikeHandler.mock.calls).toHaveLength(2)
+  })
 })
 
 
