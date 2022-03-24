4bc7e54bbc7bfc7e2301f94f3b7ffed735c2b5c3
Hanna Jantunen <hannajant@gmail.com>
2022-03-24 17:40:20 +0200

    osa 8.26

--- osa8/books-and-authors/resolvers.js
+++ osa8/books-and-authors/resolvers.js
@@ -1,144 +1,140 @@
-const { UserInputError, AuthenticationError } = require("apollo-server");
-const jwt = require("jsonwebtoken");
-const { PubSub } = require("graphql-subscriptions");
-const pubsub = new PubSub();
+const { UserInputError, AuthenticationError } = require('apollo-server')
+const jwt = require('jsonwebtoken')
+const { PubSub } = require('graphql-subscriptions')
+const pubsub = new PubSub()
 
-const Book = require("./models/book");
-const Author = require("./models/author");
-const User = require("./models/user");
+const Book = require('./models/book')
+const Author = require('./models/author')
+const User = require('./models/user')
 
 const resolvers = {
   Query: {
     bookCount: async () => Book.collection.countDocuments(),
     authorCount: async () => Author.collection.countDocuments(),
     allBooks: async (root, args) => {
-      let allBooks = await Book.find({}).populate("author");
-      if (!args.author && !args.genre) return allBooks;
+      let allBooks = await Book.find({}).populate('author')
+      if (!args.author && !args.genre) return allBooks
 
       if (args.author) {
-        allBooks = allBooks.filter((b) => b.author.name === args.author);
+        allBooks = allBooks.filter((b) => b.author.name === args.author)
       }
       if (args.genre) {
-        allBooks = allBooks.filter((b) => b.genres.includes(args.genre));
+        allBooks = allBooks.filter((b) => b.genres.includes(args.genre))
       }
-      return allBooks;
+      return allBooks
     },
     allAuthors: async () => {
-      const authors = await Author.find({});
-      return authors;
+      const authors = await Author.find({})
+      return authors
     },
     allGenres: async () => {
-      const books = await Book.find({});
+      const books = await Book.find({})
       const genres = books.reduce((genres, book) => {
         book.genres.forEach((genre) => {
-          if (!genres.includes(genre)) genres.push(genre);
-        });
-        return genres;
-      }, []);
-      return genres;
+          if (!genres.includes(genre)) genres.push(genre)
+        })
+        return genres
+      }, [])
+      return genres
     },
     me: (root, args, context) => {
-      return context.currentUser;
+      return context.currentUser
     },
   },
   Author: {
     bookCount: async (root) => {
-      const booksByAuthor = await Book.find({
-        author: root._id,
-      });
-
-      return booksByAuthor.length;
+      return root.books.length
     },
   },
   Mutation: {
     addBook: async (root, args, context) => {
-      const currentUser = context.currentUser;
+      const currentUser = context.currentUser
       if (!currentUser) {
-        throw new AuthenticationError("not authenticated");
+        throw new AuthenticationError('not authenticated')
       }
       if (!args.author) {
-        throw new UserInputError("Author required");
+        throw new UserInputError('Author required')
       }
 
       let author = await Author.findOne({
         name: args.author,
-      });
-      console.log("auth", author);
-      if (!author) {
-        newAuthor = new Author({ name: args.author });
-        author = await newAuthor.save();
-      }
-      const book = new Book({ ...args, author });
+      })
 
-      console.log('book: ', book)
+      if (!author) {
+        newAuthor = new Author({ name: args.author })
+        author = await newAuthor.save()
+      }
+      const book = new Book({ ...args, author })
 
       try {
-        await book.save();
+        await book.save()
+        author.books = author.books.concat(book)
+        await author.save()
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args,
-        });
+        })
       }
 
-      pubsub.publish("BOOK_ADDED", { bookAdded: book });
+      pubsub.publish('BOOK_ADDED', { bookAdded: book })
 
-      return book;
+      return book
     },
     editAuthor: async (root, args, context) => {
-      const currentUser = context.currentUser;
+      const currentUser = context.currentUser
 
       if (!currentUser) {
-        throw new AuthenticationError("not authenticated");
+        throw new AuthenticationError('not authenticated')
       }
 
       if (!args.name) {
-        throw new UserInputError("Author name required");
+        throw new UserInputError('Author name required')
       }
-      const author = await Author.findOne({ name: args.name });
+      const author = await Author.findOne({ name: args.name })
 
-      if (!author) return null;
+      if (!author) return null
 
-      author.born = args.setBornTo;
+      author.born = args.setBornTo
 
       try {
-        await author.save();
+        await author.save()
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args,
-        });
+        })
       }
 
-      return author;
+      return author
     },
     createUser: async (root, args) => {
-      const user = new User({ ...args });
+      const user = new User({ ...args })
 
       return user.save().catch((error) => {
         throw new UserInputError(error.message, {
           invalidArgs: args,
-        });
-      });
+        })
+      })
     },
     login: async (root, args) => {
-      const user = await User.findOne({ username: args.username });
+      const user = await User.findOne({ username: args.username })
 
-      if (!user || args.password !== "secret") {
-        throw new UserInputError("wrong credentials");
+      if (!user || args.password !== 'secret') {
+        throw new UserInputError('wrong credentials')
       }
 
       const userForToken = {
         username: user.username,
         id: user._id,
-      };
+      }
 
-      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) };
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
     },
   },
   Subscription: {
     bookAdded: {
-      subscribe: () => pubsub.asyncIterator(["BOOK_ADDED"]),
+      subscribe: () => pubsub.asyncIterator(['BOOK_ADDED']),
     },
   },
-};
+}
 
-module.exports = resolvers;
+module.exports = resolvers
