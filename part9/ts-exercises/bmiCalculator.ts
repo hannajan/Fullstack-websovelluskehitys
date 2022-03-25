f542f733a9927c47ba18ef44c46b5a85993afa03
Hanna Jantunen <hannajant@gmail.com>
2022-03-25 17:20:12 +0200

    part 9.3

--- part9/ts-exercises/bmiCalculator.ts
+++ part9/ts-exercises/bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface InputValues {
+  value1: number;
+  value2: number;
+}
+
+const parseArguments = (args: Array<string>): InputValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / Math.pow(height / 100, 2);
   if (bmi < 18.5) {
@@ -14,11 +33,12 @@ const calculateBmi = (height: number, weight: number): string => {
 };
 
 try {
-  console.log(calculateBmi(180, 74));
+  const { value1, value2 } = parseArguments(process.argv);
+  console.log(calculateBmi(value1, value2));
 } catch (error: unknown) {
   let errorMessage = 'Something went wrong.';
   if (error instanceof Error) {
-    console.log(errorMessage + ' Error: ' + error.message);
+    errorMessage = errorMessage + ' Error: ' + error.message;
   }
   console.log(errorMessage);
 }
--- part9/ts-exercises/exerciseCalculator.ts
+++ part9/ts-exercises/exerciseCalculator.ts
@@ -13,12 +13,28 @@ interface Rating {
   ratingDescription: string;
 }
 
-interface InputValues {
-  value1: Array<number>;
-  value2: number;
+interface Input {
+  input1: Array<number>;
+  input2: number;
 }
 
-const giveRating = (average: number, target: number): Rating => {
+const parseInputArguments = (args: Array<string>): Input => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  const hours = args.slice(3);
+  const input1 = <number[]>[];
+
+  hours.forEach((a) => {
+    if (isNaN(Number(a))) throw new Error('Given arguments must be numbers');
+    input1.push(Number(a));
+  });
+
+  return {
+    input1,
+    input2: Number(args[2]),
+  };
+};
+
+function giveRating(average: number, target: number): Rating {
   const percent = average / target;
   let rating;
   let ratingDescription;
@@ -26,7 +42,7 @@ const giveRating = (average: number, target: number): Rating => {
   if (percent >= 1) {
     rating = 3;
     ratingDescription = 'target met during period';
-  } else if (percent >= 0.9) {
+  } else if (percent >= 0.8) {
     rating = 2;
     ratingDescription = 'not too bad but could be better';
   } else {
@@ -35,7 +51,7 @@ const giveRating = (average: number, target: number): Rating => {
   }
 
   return { rating, ratingDescription };
-};
+}
 
 const calculateExercises = (
   hoursPerDay: Array<number>,
@@ -61,4 +77,13 @@ const calculateExercises = (
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+  const { input1, input2 } = parseInputArguments(process.argv);
+  console.log(calculateExercises(input1, input2));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong.';
+  if (error instanceof Error) {
+    errorMessage = errorMessage + ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
