fba66ade765aa4dfadaf4ec8567452358eeae349
Hanna Jantunen <hannajant@gmail.com>
2022-03-28 14:24:11 +0300

    part 9.10

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
+
 const app = express();
 app.use(express.json());
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -10,6 +12,8 @@ const PORT = 3003;
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
